657fefc37dead1a359b2c17bd1bc265d964925d1
Jibon Khan <131758098+jibon1996@users.noreply.github.com>
2023-08-29 00:17:30 +0600

    update for active and remove classe

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 
 const fags= document.querySelectorAll(".faq");
 
-
-fags.forEach((fag) => {
-    fag.addEventListener("click", () =>{
-        fag.classList.toggle("active");
+ fags.forEach((fag) => {
+        fag.addEventListener("click", () =>{
+            for(i = 0; i < fags.length ; i++){
+                if(fags[i] != fag){
+                    fags[i].classList.remove("active");
+                }else{
+                    fag.classList.toggle("active");
+                }
+            }
+            
+        });
     });
-});
